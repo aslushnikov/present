3287142a4b0fb5ecc31289382fde679c7d451247
Andrey Lushnikov <lushnikov@chromium.org>
2015-12-31 05:05:15 +0300

    some nice version

--- script.js
+++ script.js
@@ -1,50 +1,35 @@
 document.addEventListener("DOMContentLoaded", onLoaded);
 
-var img;
+var mosaic;
 var viewport;
 var width;
 var height;
 
 function onLoaded() {
-    var stub = document.querySelector("img");
     viewport = document.querySelector(".viewport");
     viewport.addEventListener("mousedown", onMouseDown);
-    viewport.addEventListener("mousemove", onMouseMove);
     width = viewport.offsetWidth;
     height = viewport.offsetHeight;
-    img = new Image();
-    img.classList.add("mosaic");
-    img.load(stub.getAttribute("full"), onLoaded);
+
+    mosaic = new Image();
+    mosaic.classList.add("mosaic");
+    mosaic.classList.add("animate");
+
+    var stub = document.querySelector(".stub");
+    mosaic.load(stub.getAttribute("src"), function() { });
+    mosaic.addEventListener("load", onLoaded);
 
     function onLoaded()
     {
-        stub.parentElement.replaceChild(img, stub);
+        stub.parentElement.replaceChild(mosaic, stub);
+        document.querySelector(".splash").remove();
     }
-}
-
-function onMouseMove(event) {
-    if (!img.classList.contains("zoom"))
-        return;
-    var x = event.offsetX;
-    var y = event.offsetY;
-    var scale = 20;
-    var offsetX = - x + x/scale;
-    var offsetY = - y + y/scale;
-
-    var matrix = new Matrix();
-    matrix.scale(scale, scale);
-    matrix.translate(offsetX, offsetY);
-
-    img.classList.add("zoom");
-    img.style.setProperty("transform", matrix.toCSS());
-    img.style.setProperty("transition-duration", "0.1s");
 }
 
 function onMouseDown(event) {
-    if (img.classList.contains("zoom")) {
-        img.classList.remove("zoom");
-        img.style.removeProperty("transform");
-        img.style.setProperty("transition-duration", "1s");
+    if (mosaic.classList.contains("zoom")) {
+        mosaic.classList.remove("zoom");
+        viewport.style.removeProperty("transform");
         return;
     }
     var x = event.offsetX;
@@ -58,7 +43,6 @@ function onMouseDown(event) {
     matrix.scale(scale, scale);
     matrix.translate(offsetX, offsetY);
 
-    img.classList.add("zoom");
-    img.style.setProperty("transform", matrix.toCSS());
-    img.style.setProperty("transition-duration", "1s");
+    mosaic.classList.add("zoom");
+    viewport.style.setProperty("transform", matrix.toCSS());
 }
